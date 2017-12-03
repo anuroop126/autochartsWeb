9c14e72a55dd94351dfbeefae81226540fbdd0bd
pendela anuroop <anuroop126@gmail.com>
2017-12-03 11:36:31 +0530

    firebase update and gitignore

---
+++ static/scripts/autocharts_scripts/bar_chart.js
@@ -0,0 +1,16 @@
+/**
+ * [bar_graph description]
+ * A class to instantiate varible to draw bar graph. 
+ */
+class bar_graph {
+  var x = 0;
+  constructor(id, data) {
+    this.id = id
+    this.data = data
+  }
+
+  draw_graph(options) {
+
+  }
+
+}
