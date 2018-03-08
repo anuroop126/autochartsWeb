1b84ed95db302a6fb575b55cc0761b2cd209efd5
pendela anuroop <anuroop126@gmail.com>
2018-03-08 21:40:31 +0530

    ADD: adding javascript files and trying to write classes

--- static/scripts/autocharts_scripts/bar_chart.js
+++ static/scripts/autocharts_scripts/bar_chart.js
@@ -1,16 +1,4 @@
+//TODO: Create a class and objectify it.
 /**
- * [bar_graph description]
- * A class to instantiate varible to draw bar graph. 
+ *
  */
-class bar_graph {
-  var x = 0;
-  constructor(id, data) {
-    this.id = id
-    this.data = data
-  }
-
-  draw_graph(options) {
-
-  }
-
-}
